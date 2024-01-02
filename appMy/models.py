033092699cec2b6dbd128fd5ef950a3c9859d27d
Ali ALİN <mehmettalnn@gmail.com>
2024-01-02 15:30:48 +0300

    a

--- appMy/models.py
+++ appMy/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(models.Model):
     title =models.CharField(("Başlik"), max_length=50)
     alt_title1 = models.CharField(("Alt Başlık1"), max_length=50, null =True, blank = True)
     alt_title2 = models.CharField(("Alt Başlık2"), max_length=50, null =True, blank = True)
-    alt_title3 = models.CharField(("Alt Başlık3"), max_length=50, null =True, blank = True) 
+    alt_title3= models.CharField(("Alt Başlık3"), max_length=50, null =True, blank = True) 
+    alt_title4 = models.CharField(("Alt Başlık4"), max_length=50, null =True, blank = True) 
+    alt_title5 = models.CharField(("Alt Başlık5"), max_length=50, null =True, blank = True) 
+    alt_title6 = models.CharField(("Alt Başlık6"), max_length=50, null =True, blank = True) 
     slug = models.SlugField(("Slug"))
     
     def __str__(self):
         return self.title
-    
+
+
 class Kurs(models.Model):
     user = models.ForeignKey(User, related_name = "user1", verbose_name=("Kullanıcı"), on_delete=models.CASCADE)
     category = models.ForeignKey(Category, verbose_name=("Kategori"), null =True, on_delete=models.CASCADE)

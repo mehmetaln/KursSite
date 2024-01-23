6745f74e9ca07128919090dc546a214b2346b881
Ali ALİN <mehmettalnn@gmail.com>
2024-01-23 09:26:16 +0300

    a

--- KursSite/settings.py
+++ KursSite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-of-cy75y=ywqzyr7!*nhj3o0-1x14y9a_cbackn5pq8y6zh0#w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://mehmetalnn.pythonanywhere.com']
 
 
 # Application definition
